a6152d5b4e6462351dc53cbb159d44714f2d15e3
Michał Białek <kontakt.michalbialek@gmail.com>
2024-09-10 11:43:03 +0200

    feat: Providing Header and Contact components

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+// rsc, rsf  - wiecej w abbreviation settings
+
+
+import React from 'react';
+
+const Header = ({toggleModal, numberOfContacts}) => {
+    return (
+        <header className='header'>
+            <div className='container'>
+                <h3>Contact List ({numberOfContacts})</h3>
+                <button className='btn' onClick={() => {toggleModal(true)}}>
+                    <i className='bi bi-plus-square'></i>New Contact</button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
+
